e40266de7398b8354007a1f1faf356359be28fd9
fanyingmao <646900491@qq.com>
2018-05-22 23:47:11 +0800

    learn

--- .roadhogrc.mock.js
+++ .roadhogrc.mock.js
@@ -1,3 +1,6 @@
 
 export default {
+  theme: {
+    "primary-color": "#ECECEC"
+  }
 };
--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 import IndexPage from './routes/IndexPage';
 import Products from './routes/Products';
+import Flex from './routes/Flex';
+import Layout from './routes/Layout';
 
 function RouterConfig({ history }) {
   return (
@@ -9,6 +11,8 @@ function RouterConfig({ history }) {
       <Switch>
         <Route path="/" exact component={IndexPage} />
         <Route path="/products" exact component={Products} />
+        <Route path="/flex" exact component={Flex} />
+        <Route path="/layout" exact component={Layout} />
       </Switch>
     </Router>
   );
