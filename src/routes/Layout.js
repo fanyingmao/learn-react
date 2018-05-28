debc8967aa38aa2196332f49bb4b6a2c685b10ec
fanyingmao <646900491@qq.com>
2018-05-28 23:02:53 +0800

    link

--- src/routes/Layout.js
+++ src/routes/Layout.js
@@ -1,50 +1,26 @@
 // Flex.js
 // Created by fanyingmao 五月/20/2018
 //
-import { Layout } from 'antd';
-import {connect} from 'dva';
-
+import { Layout } from "antd";
+import { connect } from "dva";
+import { Link, Redirect, Switch, Route } from "dva/router";
 const { Header, Footer, Sider, Content } = Layout;
 
-
 const mLayout = () => {
-
   return (
     <div>
       <Layout>
         <Header>Header</Header>
-        <Content>Content</Content>
-        <Footer>Footer</Footer>
-      </Layout>
-
-      <Layout>
-        <Header>Header</Header>
         <Layout>
           <Sider>Sider</Sider>
           <Content>Content</Content>
         </Layout>
-        <Footer>Footer</Footer>
-      </Layout>
-
-      <Layout>
-        <Header>Header</Header>
-        <Layout>
-          <Content>Content</Content>
-          <Sider>Sider</Sider>
-        </Layout>
-        <Footer>Footer</Footer>
-      </Layout>
-
-      <Layout>
-        <Sider>Sider</Sider>
-        <Layout>
-          <Header>Header</Header>
-          <Content>Content</Content>
+        <Link to="/Products">
           <Footer>Footer</Footer>
-        </Layout>
+        </Link>
       </Layout>
-    </div>);
+    </div>
+  );
 };
-
 
 export default connect()(mLayout);
